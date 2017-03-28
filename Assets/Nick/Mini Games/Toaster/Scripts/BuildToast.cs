ef3ac9f70375c4de4da308d4e057152e312b6a26
Nick Whelan <nickwhelan5@gmail.com>
2017-03-28 19:39:54 -0400

    Toaster Started

---
+++ Assets/Nick/Mini Games/Toaster/Scripts/BuildToast.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildToast : MonoBehaviour {
+
+    public GameObject BreadCube;
+    public List<GameObject> BreadCubes;
+    public Transform BreadCubesParent;
+
+    public int BreadWidth, BreadHeight;
+
+	// Use this for initialization
+	void Start () {
+
+        float startX = transform.position.x - (BreadWidth/2) + 0.5f;
+        float startY = transform.position.y + (BreadHeight/2) + 0.5f;
+        int w = 0;
+        for (int i = 0; i < BreadWidth * BreadHeight; i++) {
+            BreadCubes.Add(Instantiate(BreadCube));
+            BreadCubes[i].transform.parent = BreadCubesParent;
+            BreadCubes[i].name = "BreadCube ("+i +") "+ (int) startX + " " + (int) startY;
+            BreadCubes[i].transform.position = new Vector3(startX, startY, transform.position.z);
+            BreadCubes[i].transform.localScale = new Vector3(1, 1, 1);
+
+            if (this.gameObject.layer == LayerMask.NameToLayer("Team 1")) {
+                BreadCubes[i].GetComponent<BoxCollider>().center = new Vector3(0, 0, -2);
+            }
+
+            startX += BreadCubes[i].transform.localScale.x;
+            if (w >= BreadWidth-1)
+            {
+                w = 0;
+                startX = transform.position.x - (BreadWidth / 2) + 0.5f;
+                startY -= BreadCubes[i].transform.localScale.y;
+            }
+            else {
+                w++;
+            }
+            
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
